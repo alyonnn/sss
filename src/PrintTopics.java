8a2b9328d31ac4b383b248e2bf427dd7a927b724
alyonnn <stilk8@gmail.com>
2025-05-14 12:45:58 +0300

    init test

--- src/PrintTopics.java
+++ src/PrintTopics.java
@@ -14,6 +14,9 @@ public class PrintTopics {
         }
     }
 
+
+
+
     public static void main(String[] args) {
         System.out.println("Вывод на русском:");
         printTopics(new Locale("ru", "RU"));
